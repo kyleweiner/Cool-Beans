d35e0021bcbea7e41554cd4ec57cc46d866f8c61
Kyle Weiner <kyle@kylemade.com>
2016-03-24 00:40:31 -0400

    Update info URL

--- Cool Beans/Controllers/InfoViewController.swift
+++ Cool Beans/Controllers/InfoViewController.swift
@@ -7,7 +7,7 @@ import UIKit
 
 class InfoViewController: UIViewController {
     @IBAction func openURL(sender: AnyObject) {
-        let url = NSURL(string: "http://git.io/Xi6unA")
+        let url = NSURL(string: "https://goo.gl/mAIzHw")
         UIApplication.sharedApplication().openURL(url!)
     }
 
